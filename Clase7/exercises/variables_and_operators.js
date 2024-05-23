96d20d7b8e3b1e1feb76805038e65dde5a4df7a0
ArcNeb <facufarneda@gmail.com>
2024-05-23 16:01:30 -0300

    Aparently unnecesary files were deleted from the folder Clase6. Clase7's first exercise is now completed and second is on progress.

---
+++ Clase7/exercises/variables_and_operators.js
@@ -0,0 +1,35 @@
+/*a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor
+de la suma de ambos números en una 3er variable.*/
+
+var numero1 = 9
+var numero2 = 10
+
+var numero3 = numero1 + numero2
+
+console.log(numero3)
+
+/*b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una
+3er variable.*/
+
+var cadena1 = "Cadena1"
+var cadena2 = "Cadena2"
+
+var cadena3 = cadena1.concat(cadena2)
+
+console.log(cadena3)
+
+/*c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de
+letras del string) guardando el resultado de la suma en una 3er variable (utilizar
+length)*/
+
+var cadena4 = "Cadena4"
+var cadena5 = "Cadena5"
+
+var LargoCadena4 = cadena4.length
+var LargoCadena5 = cadena5.length
+
+var resultado = LargoCadena4 + LargoCadena5
+
+console.log(resultado)
+
+console.log("---------------------")
