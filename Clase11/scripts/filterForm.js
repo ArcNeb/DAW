44a1397dd934b2dc0f2a903583a4c6167fc5e5d5
ArcNeb <facufarneda@gmail.com>
2024-06-26 20:16:54 -0300

    Now it's possible to reset the filter form when closing it

--- Clase11/scripts/filterForm.js
+++ Clase11/scripts/filterForm.js
@@ -1,8 +1,46 @@
 document.getElementById("FilterCharactersButton").addEventListener("click", function (event) {
     document.getElementById("BackgroundForm").style.display = "flex"
+
+    document.getElementById("FieldName").getElementsByTagName("input")[0].value = ""
+
+    document.getElementById("FieldType").getElementsByTagName("input")[0].value = ""
+
+    var estadosPosibles = document.getElementsByName("status")
+    estadosPosibles.forEach(element => {
+        element.checked = false
+    })
+
+    var especiesPosibles = document.getElementsByName("specie")
+    especiesPosibles.forEach(element => {
+        element.checked = false
+    })
+
+    var generosPosibles = document.getElementsByName("gender")
+    generosPosibles.forEach(element => {
+        element.checked = false
+    })
 })
 
 document.getElementById("CloseFilter").addEventListener("click", function (event) {
     document.getElementById("BackgroundForm").style.display = "none"
+
+    document.getElementById("FieldName").getElementsByTagName("input")[0].value = ""
+
+    document.getElementById("FieldType").getElementsByTagName("input")[0].value = ""
+
+    var estadosPosibles = document.getElementsByName("status")
+    estadosPosibles.forEach(element => {
+        element.checked = false
+    })
+
+    var especiesPosibles = document.getElementsByName("specie")
+    especiesPosibles.forEach(element => {
+        element.checked = false
+    })
+
+    var generosPosibles = document.getElementsByName("gender")
+    generosPosibles.forEach(element => {
+        element.checked = false
+    })
 })
 
