1b8aac69b77e8454174a2770c940059aa66a57ae
ArcNeb <facufarneda@gmail.com>
2024-06-27 12:40:10 -0300

    filterForm's script was summarized

--- Clase11/scripts/filterForm.js
+++ Clase11/scripts/filterForm.js
@@ -1,29 +1,16 @@
 document.getElementById("FilterCharactersButton").addEventListener("click", function (event) {
     document.getElementById("BackgroundForm").style.display = "flex"
 
-    document.getElementById("FieldName").getElementsByTagName("input")[0].value = ""
-
-    document.getElementById("FieldType").getElementsByTagName("input")[0].value = ""
-
-    var estadosPosibles = document.getElementsByName("status")
-    estadosPosibles.forEach(element => {
-        element.checked = false
-    })
-
-    var especiesPosibles = document.getElementsByName("specie")
-    especiesPosibles.forEach(element => {
-        element.checked = false
-    })
-
-    var generosPosibles = document.getElementsByName("gender")
-    generosPosibles.forEach(element => {
-        element.checked = false
-    })
+    reiniciarPagina()
 })
 
 document.getElementById("CloseFilter").addEventListener("click", function (event) {
     document.getElementById("BackgroundForm").style.display = "none"
 
+    reiniciarPagina()
+})
+
+function reiniciarPagina() {
     document.getElementById("FieldName").getElementsByTagName("input")[0].value = ""
 
     document.getElementById("FieldType").getElementsByTagName("input")[0].value = ""
@@ -42,5 +29,5 @@ document.getElementById("CloseFilter").addEventListener("click", function (event
     generosPosibles.forEach(element => {
         element.checked = false
     })
-})
+}
 
